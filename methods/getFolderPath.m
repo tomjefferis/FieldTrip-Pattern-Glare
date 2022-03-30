09557d7b6c556fa73b748a54d66a3fdd6efe18e4
Tom Jefferis <tomjefferis@gmail.com>
2022-03-30 14:13:04 +0100

    grad updates

--- methods/getFolderPath.m
+++ methods/getFolderPath.m
@@ -9,12 +9,14 @@ function [results_dir, main_path] = getFolderPath()
         main_path = '/Users/tomjefferis/Documents/PhD/EEG Data/participants/participant_';
         addpath('/Users/tomjefferis/Documents/PhD/MatlabPlugins/fieldtrip-20210906'); % path to fieldtrip
         addpath('/Users/tomjefferis/Documents/PhD/MatlabPlugins/spm12') % path to spm
+        addpath("/Users/tomjefferis/Library/Application Support/MathWorks/MATLAB Add-Ons/Functions/Patchline/") %path to patchline plot
     elseif ispc
         % Code to run on Windows platform
         results_dir = 'W:\PhD\PatternGlareData\Results'; % path to results
         main_path = 'W:\PhD\PatternGlareData\participants\participant_';
         addpath('W:\PhD\MatlabPlugins\fieldtrip-20210906'); % path to fieldtrip
         addpath('W:\PhD\MatlabPlugins\spm12') % path to spm
+        addpath("C:\Users\Tom\AppData\Roaming\MathWorks\MATLAB Add-Ons\Functions\Patchline") 
     else
         fprintf('Unsupported System');
     end
