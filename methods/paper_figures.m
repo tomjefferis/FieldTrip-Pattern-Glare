65c1e3b0a946f70b8c2a79cc709e33e6db745627
Tom Jefferis <10071295+tomjefferis@users.noreply.github.com>
2023-03-08 16:55:13 +0000

    Update paper_figures.m

--- methods/paper_figures.m
+++ methods/paper_figures.m
@@ -1,11 +1,18 @@
 function paper_figures(data, stat, design, onsets_part, figname, start_time, end_time, results)
-figname = paper_figure_name(figname, onsets_part,start_time, end_time);
+
+    figname = paper_figure_name(figname, onsets_part, start_time, end_time);
 
     % if data is struct assign each element to a variable
     if isstruct(data)
         data1 = data.one;
         data2 = data.two;
         data3 = data.three;
+    end
+
+    if isstruct(design)
+        design1 = design.one;
+        design = design.two;
+        design3 = design.three;
     end
 
     factor = figname;
@@ -35,14 +42,14 @@ figname = paper_figure_name(figname, onsets_part,start_time, end_time);
     Cluster_vol = print('-RGBImage');
     Topomap = plot_topo_map(stat, start_time, end_time, Effect_direction, factor, '', true);
     Topomap = print('-RGBImage');
-    Elec_View = plot_peak_electrode(stat,electrode , '', true);
+    Elec_View = plot_peak_electrode(stat, electrode, '', true);
     Elec_View = print('-RGBImage');
-    
-    
+
     if contains(onsets_part, 'onsets') || contains(onsets_part, 'onset 1') || contains(onsets_part, 'Partition 1')
-        if ~contains(factor,'none')
+
+        if ~contains(factor, 'none')
             [low, high] = median_split(data, 1, design);
-            erpplot = plot_medium_split(high, low, electrode, factor, start_time, end_time, true, '',true);
+            erpplot = plot_medium_split(high, low, electrode, factor, start_time, end_time, true, '', true);
             erpplot = print('-RGBImage');
         else
             %subplots of pgi and erp
@@ -50,37 +57,46 @@ figname = paper_figure_name(figname, onsets_part,start_time, end_time);
             erpplot = print('-RGBImage');
             pgiplot = generate_erp_pgi('', start_time, end_time, data, electrode, factor, true, Effect_direction, true);
             pgiplot = print('-RGBImage');
-            erpplot = imstacker({erpplot,pgiplot},'dim',1,'padding',[255,255,255],'gravity','center');
+            erpplot = imstacker({erpplot, pgiplot}, 'dim', 1, 'padding', [255, 255, 255], 'gravity', 'center');
         end
+
     elseif contains(onsets_part, 'Partitions')
-        if ~contains(factor,'none')
+
+        if ~contains(factor, 'none')
             erpplot = plot_partitions_erp(data1, data2, data3, electrode, design, factor, '', start_time, end_time, true, true);
             erpplot = print('-RGBImage');
         else
             erpplot = plot_partitions_regressor(data1, data2, data3, electrode, design, factor, '', start_time, end_time, true, true);
             erpplot = print('-RGBImage');
         end
+
     elseif contains(onsets_part, 'Onsets 2,3 vs 4,5 vs 6,7')
-        if ~contains(factor,'none')
+
+        if ~contains(factor, 'none')
             erpplot = plot_partitions_erp(data1, data2, data3, electrode, design, factor, '', start_time, end_time, true, true);
             erpplot = print('-RGBImage');
         else
             erpplot = plot_partitions_regressor(data1, data2, data3, electrode, design, factor, '', start_time, end_time, true, true);
             erpplot = print('-RGBImage');
         end
+
     elseif contains(onsets_part, 'Partitions vs Onsets')
         erpplot = plot_three_way(data1, data2, data3, electrode, design, factor, '', start_time, end_time, true, true);
         erpplot = print('-RGBImage');
     end
-    
-    im1 = {Cluster_vol,Elec_View};
-    outpict = imstacker(im1,'dim',2,'padding',[255,255,255],'gravity','center');
 
-    images = {outpict,Topomap,erpplot};
+    im1 = {Cluster_vol, Elec_View};
+    outpict = imstacker(im1, 'dim', 2, 'padding', [255, 255, 255], 'gravity', 'center');
 
-    outpict = imstacker(images,'dim',1,'padding',[255,255,255],'gravity','center');
+    images = {outpict, Topomap, erpplot};
+
+    outpict = imstacker(images, 'dim', 1, 'padding', [255, 255, 255], 'gravity', 'center');
     figure;
     imshow(outpict)
     title(figname);
+
+    %save figure to results folder named after the figure
+    cd (results + "/figures");
+    saveas(gcf, figname, 'png');
 
 end
