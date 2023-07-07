f73cc977d24e6fbe094f9293ccc4b118d1ab5fb0
Tom Jefferis <tomjefferis@gmail.com>
2023-07-07 13:51:23 +0100

    freq padding fixes

--- main.m
+++ main.m
@@ -22,10 +22,10 @@ grand_avg_eyes = 'time_domain_eye_confound_onsets_2_3_4_5_6_7_8_grand-average.ma
 %% Experiment parameters
 % ROI window
 %time_window = [3.09, 3.18; 3.18, 3.45; 3.45, 3.83];
-time_window = [3.09 3.18];
-n_participants = 40;
-%baseline_period = [-0.2 0];
-baseline_period = [2.8 3.0];
+time_window = [0.5 3.05];
+n_participants = 4 ;
+baseline_period = [-0.2 0];
+%baseline_period = [2.8 3.0];
 aggregated_roi = false; % uses aggregated average approach to find the ROI
 max_windows = 4; % maximum amount of windows the roi finder finds
 spatial_roi = false; % generate a spatial region of interest - not useful for most of these analysis ----- NEED TO FIX FOR FREQ
@@ -34,9 +34,9 @@ posneg = false; %true = positive side of roi false = negative
 % if need statistical test without plotting
 stat_run = true;
 %% frequency config
-wavelet_width = 3;
+wavelet_width = 5;
 decimate = 256; % this is the frequency domain resampling for memory efficiancy, this should be calculated using nyquist 
-frequency_range = [20 35]; % start and end frequency for stat tests
+frequency_range = [8 13]; % start and end frequency for stat tests
 power_itc = 'pow'; %looking at power ot itc options: pow, itc
 %% Plotting config
 clust_volume = false; % cluter volume over time
@@ -49,9 +49,9 @@ plot_designs = false; %plots design matrix for partitions ONLY
 plot_partitions_erps = false; % 10x2 figure of median split partitions for factor
 generate_ci = false; % do we want confidence intervals !!BREAKS MEDIAN SPLIT PLOTS AND PARTITION SPLIT IF FALSE!!
 %% generate experiment dsign
-time_freq = 'time'; % time or frequency domain options: time or frequency
+time_freq = 'frequency'; % time or frequency domain options: time or frequency
 factor_scores = {'discomfort'}; % options: none, headache, visual-stress, discomfort, all
-onsets_part = 'partitions'; % options: onsets, partitions, onsets-23-45-67, eyes, partition1, partitions-vs-onsets
+onsets_part = 'onsets'; % options: onsets, partitions, onsets-23-45-67, eyes, partition1, partitions-vs-onsets
 type_of_effect = {'habituation'}; % habituation or sensitization
 three_way_type = {'habituation'}; % same as previous but only used when making the 3 way comparison
 partitions = 'orthog'; % orthogonolize design matrix for partitions (zero center), options: normal, orthog
@@ -70,6 +70,12 @@ paper_figs = true;
 %               plot_erps,median_split_plots,gfp_plot,plot_designs,plot_partitions_erps,generate_ci,time_freq,factor_scores, ...
 %               onsets_part, type_of_effect, testing)
 %
+
+if baseline_period(1) == -0.2
+    wavelet_width = round(0.1447 * round((frequency_range(1)+frequency_range(2))/2)*pi);
+else
+    wavelet_width = round(0.1 * round((frequency_range(1)+frequency_range(2))/2)*pi);
+end
 
 tab = pgi_analysis(grand_avg_filename, single_trial_freq_filename, grand_avg_partitions_filename, single_trial_freq_partitions_filename, time_window, n_participants, baseline_period, ...
 aggregated_roi, max_windows, spatial_roi, posneg, stat_run, wavelet_width, frequency_range, decimate, power_itc, tfr_plots, clust_volume, topograpic_map_plot, ...
--- methods/pad_series.m
+++ methods/pad_series.m
@@ -1,10 +1,20 @@
 function data = pad_series(data, pad_samp, step)
+
+    time_Start = 1;
+    [~, time_End] = min(abs(data{1}.time));
+    time_End = time_End-1;
+    timelen = time_End - time_Start + 1;
+
     for index = 1:numel(data)
         data1 = data{index}.avg;
         data_time = data{index}.time;
-        data{index}.time = linspace(data_time(1)-step*pad_samp, data_time(end)+step*pad_samp, length(data_time)+2*pad_samp);
+        data{index}.time = linspace(data_time(1)-4*timelen*step, data_time(end)+pad_samp*step, length(data_time)+4*timelen+pad_samp);
+
     for av = 1:numel(data1)
-        data1{av} = ft_preproc_padding(data1{av},'mirror', pad_samp, pad_samp);
+        pad_data = data1{av}(:,time_Start:time_End);
+        pad_mirror = fliplr(pad_data);
+        data1{av} = [pad_data,pad_mirror,pad_data,pad_mirror,data1{av}];
+        data1{av} = ft_preproc_padding(data1{av},'mirror', 0, pad_samp);
     end
     data{index}.avg = data1;
 end
